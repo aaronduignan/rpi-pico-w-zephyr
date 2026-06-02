4e5382450ed8dd425873d673529d5c02a05e0f2e
Aaron Duignan <aaron.duignan@gmail.com>
2026-06-02 10:30:58 +0100

    Tune VS Code build workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,10 @@
 # Run inside the container: docker compose run zephyr ./scripts/build.sh
 set -e
 
-JOBS=${JOBS:-4}
+JOBS=${JOBS:-6}
+PRISTINE=${PRISTINE:-auto}
 
 export ZEPHYR_BASE=/workspace/zephyr
 cd /workspace
-west build -b rpi_pico/rp2040/w /workspace/app --build-dir /workspace/app/build --pristine=auto --build-opt=-j"${JOBS}" "$@"
+west build -b rpi_pico/rp2040/w /workspace/app --build-dir /workspace/app/build --pristine="${PRISTINE}" --build-opt=-j"${JOBS}" "$@"
 echo "Build complete: app/build/zephyr/zephyr.elf"
